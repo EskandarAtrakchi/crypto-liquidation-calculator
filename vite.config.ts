f9df47e58e522bddd56eb526d59de176e09c2e6a
EskandarAtrakchi <isgan151@gmail.com>
2024-11-03 22:16:57 +0000

    port fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    port: parseInt(process.env.PORT || "5173"), // Fallback to 5173 if PORT is not defined
+  },
   plugins: [react()],
   resolve: {
     alias: {
